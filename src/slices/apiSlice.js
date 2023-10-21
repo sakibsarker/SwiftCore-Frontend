bfcb6c2ec218d7b44461a381da0161e1a33ddb80
sakibsarker <sakib15-3824@diu.edu.bd>
2023-10-21 13:03:39 +0600

    cor use

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,7 +1,10 @@
 import {BASE_URL} from '../constants'
 import { createApi,fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const baseQuery=fetchBaseQuery({baseUrl:BASE_URL});
+const baseQuery=fetchBaseQuery({
+    baseUrl:BASE_URL,
+    credentials: 'include',
+});
 
 export const apiSlice=createApi({
     baseQuery,
