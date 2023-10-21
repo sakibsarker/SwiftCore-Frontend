8f82a4d26add507ed1607e99c1f99c44475698b8
sakibsarker <sakib15-3824@diu.edu.bd>
2023-10-21 13:28:34 +0600

    auth slice

--- src/slices/authSlice.js
+++ src/slices/authSlice.js
@@ -1,39 +1,33 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    userInfo: localStorage.getItem("userInfo")
-      ? JSON.parse(localStorage.getItem("userInfo"))
-      : null,
-    mode: localStorage.getItem("mode") ? localStorage.getItem("mode") : null,
-  };
+  userInfo: localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null,
+  mode: localStorage.getItem("mode") ? localStorage.getItem("mode") : null,
+};
 
-const authSlice=createSlice({
-    name:'auth',
-    initialState,
-    reducers:{
-        setCredentials:(state,action)=>{
-            state.userInfo=action.payload
-            localStorage.setItem('userInfo',JSON.stringify(action.payload))
-            // localStorage.setItem('token', JSON.stringify(action.payload.token))
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setCredentials: (state, action) => {
+      state.userInfo = action.payload;
+      localStorage.setItem("userInfo", JSON.stringify(action.payload));
+      // localStorage.setItem('token', JSON.stringify(action.payload.token))
+    },
+    logout: (state, action) => {
+      state.userInfo = null;
+      localStorage.removeItem("userInfo");
+    },
+    setMode: (state, action) => {
+      state.mode = action.payload;
+      console.log("Payload: ", action.payload);
+      localStorage.setItem("mode", action.payload);
+    },
+  },
+});
 
-            
+export const { setCredentials, logout, setMode } = authSlice.actions;
 
-        },
-        logout:(state,action)=>{
-            state.userInfo=null;
-            localStorage.removeItem('userInfo');
-
-           
-           
-        },
-        setMode: (state, action) => {
-            state.mode = action.payload;
-            console.log("Payload: ", action.payload);
-            localStorage.setItem("mode", action.payload);
-          },
-    }
-})
-
-export const{setCredentials,logout}=authSlice.actions;
-
-export default authSlice.reducer
+export default authSlice.reducer;
